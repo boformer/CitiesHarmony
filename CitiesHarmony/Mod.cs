976a726b4a1d9013d18a7c99d36ef6b62017144b
Felix <felix.free@gmx.de>
2020-05-24 04:46:59 +0300

    put new version numbers in a few places

--- CitiesHarmony/Mod.cs
+++ CitiesHarmony/Mod.cs
@@ -2,7 +2,7 @@
 
 namespace CitiesHarmony {
     public class Mod : IUserMod {
-        public string Name => $"Harmony 2.0.0.9";
+        public string Name => $"Harmony 2.0.1";
 
         public string Description => "Mod Dependency";
     }
