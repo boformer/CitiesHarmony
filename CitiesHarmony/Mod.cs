354b9563ec961cb79512c978113be75d5190c70a
Felix <felix.free@gmx.de>
2021-01-31 18:36:04 +0200

    rename 0Harmony assembly to CitiesHarmony.0Harmony, remove IUserMod static ctor

--- CitiesHarmony/Mod.cs
+++ CitiesHarmony/Mod.cs
@@ -1,12 +1,14 @@
-﻿using ICities;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using ColossalFramework.PlatformServices;
+using ICities;
 
 namespace CitiesHarmony {
     public class Mod : IUserMod {
         public string Name => $"Harmony 2.0.4";
 
         public string Description => "Mod Dependency";
-
-        // Install Harmony as soon as possible to avoid problems with mods not following the guidelines
-        static Mod() => Installer.Run();
     }
 }
