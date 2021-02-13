b17cccb3802afdd9e22d1a09dc619b07b3d72252
Felix <felix.free@gmx.de>
2021-02-14 00:37:23 +0200

    add AssemblyResolveLogger project

---
+++ CitiesHarmony.AssemblyResolveLogger/Mod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using ICities;
+
+namespace CitiesHarmony.AssemblyResolveLogger
+{
+    public class Mod : IUserMod
+    {
+        public string Name => "AssemblyResolveLogger";
+        public string Description => "";
+
+        public void OnEnabled() {
+            UnityEngine.Debug.Log($"Installing AssemblyResolveLogger");
+
+            var vanillaResolver = typeof(BuildConfig).GetMethod("CurrentDomain_AssemblyResolve", BindingFlags.NonPublic | BindingFlags.Static);
+            ResolveEventHandler dCSResolver = (ResolveEventHandler)Delegate.CreateDelegate(typeof(ResolveEventHandler), vanillaResolver);
+
+            AppDomain.CurrentDomain.AssemblyResolve -= dCSResolver;
+            AppDomain.CurrentDomain.TypeResolve -= dCSResolver;
+            AppDomain.CurrentDomain.AssemblyResolve += LogAssemblyResolve;
+            AppDomain.CurrentDomain.TypeResolve += LogAssemblyResolve;
+            AppDomain.CurrentDomain.AssemblyResolve += dCSResolver;
+            AppDomain.CurrentDomain.TypeResolve += dCSResolver;
+        }
+
+        public void OnDisabled() {
+            UnityEngine.Debug.Log($"Uninstalling AssemblyResolveLogger");
+
+            AppDomain.CurrentDomain.AssemblyResolve -= LogAssemblyResolve;
+            AppDomain.CurrentDomain.TypeResolve -= LogAssemblyResolve;
+        }
+
+        private static Assembly LogAssemblyResolve(object sender, ResolveEventArgs args) {
+            if (args.Name.Contains("0Harmony")) {
+                UnityEngine.Debug.Log($"[AssemblyResolveLogger] Resolving '{args.Name}' from:\n{Environment.StackTrace}");
+            }
+            return null;
+        }
+    }
+}
