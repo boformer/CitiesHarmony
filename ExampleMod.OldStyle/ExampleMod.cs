88003e20a79e7250b7bfda90bbb52c9590adf1fc
Felix <felix.free@gmx.de>
2021-02-14 06:46:46 +0200

    add "old csproj style" example mod

---
+++ ExampleMod.OldStyle/ExampleMod.cs
@@ -0,0 +1,23 @@
+﻿using CitiesHarmony.API;
+using ICities;
+// Make sure that "using HarmonyLib;" does not appear here!
+// Only reference HarmonyLib in code that runs when Harmony is ready (DoOnHarmonyReady, IsHarmonyInstalled)
+
+namespace ExampleMod {
+    public class Mod : IUserMod {
+        // Make sure that HarmonyLib is not referenced in any way in your IUserMod implementation!
+        // Instead, apply your patches from a separate static patcher class!
+        // (otherwise it will fail to instantiate the type when CitiesHarmony is not installed)
+
+        public string Name => "Harmony 2 Example";
+        public string Description => "Patches SimulationManager.CreateRelay and LoadingManager.MetaDataLoaded";
+
+        public void OnEnabled() {
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+        }
+
+        public void OnDisabled() {
+            if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
+        }
+    }
+}
---
+++ ExampleMod.OldStyle/Patcher.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace ExampleMod {
+    public static class Patcher {
+        private const string HarmonyId = "boformer.Harmony2Example";
+
+        private static bool patched = false;
+
+        public static void PatchAll() {
+            if (patched) return;
+
+            UnityEngine.Debug.Log("Harmony 2 Example: Patching...");
+
+            patched = true;
+
+            // Apply your patches here!
+            // Harmony.DEBUG = true;
+            var harmony = new Harmony("boformer.Harmony2Example");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void UnpatchAll() {
+            if (!patched) return;
+
+            var harmony = new Harmony(HarmonyId);
+            harmony.UnpatchAll(HarmonyId);
+
+            patched = false;
+
+            UnityEngine.Debug.Log("Harmony 2 Example: Reverted...");
+        }
+    }
+
+    // Random example patch
+    [HarmonyPatch(typeof(SimulationManager), "CreateRelay")]
+    public static class SimulationManagerCreateRelayPatch {
+        public static void Prefix() {
+            UnityEngine.Debug.Log("CreateRelay Prefix");
+        }
+    }
+
+    // Random example patch
+    [HarmonyPatch(typeof(LoadingManager), "MetaDataLoaded")]
+    public static class LoadingManagerMetaDataLoadedPatch {
+        public static void Prefix() {
+            UnityEngine.Debug.Log("MetaDataLoaded Prefix");
+        }
+    }
+}
