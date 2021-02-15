9c57228f0e9644cf959c68b6998c650b5c9d5c0b
Felix Schmidt <felix.free@gmx.de>
2021-02-15 23:58:28 +0200

    add domain poking mod to consistently reproduce the error caused by NExt2

---
+++ CitiesHarmony.DomainPoker/Mod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using ICities;
+
+namespace CitiesHarmony.DomainPoker {
+    public class Mod : IUserMod {
+        public string Name => "DomainPoker";
+        public string Description => "";
+
+        public void OnEnabled() {
+            UnityEngine.Debug.Log($"[DomainPoker] Get ready to be poked!");
+
+            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
+                try {
+                    UnityEngine.Debug.Log($"Calling GetExportedTypes() for {asm}");
+                    asm.GetExportedTypes();
+                    UnityEngine.Debug.Log($"GetExportedTypes successful!");
+
+                    UnityEngine.Debug.Log($"Calling GetTypes() for {asm}");
+                    asm.GetTypes();
+                    UnityEngine.Debug.Log($"GetTypes successful!");
+                } catch (Exception e) {
+                    UnityEngine.Debug.LogException(e);
+                }
+            }
+        }
+    }
+}
