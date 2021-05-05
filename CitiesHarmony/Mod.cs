d1d44fda75c8735e735e6efe2e045d346a58cf5c
Felix Schmidt <felix.free@gmx.de>
2021-05-05 10:22:49 +0200

    Catch exceptions when reapplying Harmony 1 patches, add additional logging

--- CitiesHarmony/Mod.cs
+++ CitiesHarmony/Mod.cs
@@ -2,7 +2,7 @@
 
 namespace CitiesHarmony {
     public class Mod : IUserMod {
-        public string Name => $"Harmony 2.0.4-3";
+        public string Name => $"Harmony 2.0.4-4";
 
         public string Description => "Mod Dependency";
     }
