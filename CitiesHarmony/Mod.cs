b5b6563d5d52cacbee47271c2fb9e1862609059f
boformer <boformer@users.noreply.github.com>
2022-11-16 11:10:44 +0100

    use static constructor to run installer logic because IUserMods might be constructed multiple times

--- CitiesHarmony/Mod.cs
+++ CitiesHarmony/Mod.cs
@@ -7,7 +7,7 @@ namespace CitiesHarmony
         public string Name => $"Harmony 2.2.2-0";
         public string Description => "Mod Dependency";
 
-        public Mod()
+        static Mod()
         {
             // Try to patch all Harmony 1 assemblies before they are used for patching
             // (this is possible due to a change in 1.15.1-f4 that constructs all mods first before calling OnEnabled on any of them)
